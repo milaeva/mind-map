855e30b4e746c16923beeefdaf3a0a70e8e7398a
Vita Mileva <milayevavita27@gmail.com>
2020-12-15 07:57:59 +0200

    create mind-map for books

---
+++ main.js
@@ -0,0 +1,326 @@
+// Модальное окно
+
+// открыть по кнопке
+$(".js-button-campaign-1").click(function () {
+  $(".js-overlay-campaign-1").fadeIn();
+  $(".js-overlay-campaign-1").addClass("disabled");
+});
+
+// закрыть на крестик
+$(".js-close-campaign-1").click(function () {
+  $(".js-overlay-campaign-1").fadeOut();
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) {
+  var popup = $(".js-popup-campaign-1");
+  if (e.target != popup[0] && popup.has(e.target).length === 0) {
+    $(".js-overlay-campaign-1").fadeOut();
+  }
+});
+
+$(".js-button-campaign-2").click(function () {
+  $(".js-overlay-campaign-2").fadeIn();
+  $(".js-overlay-campaign-2").addClass("disabled");
+});
+$(".js-close-campaign-2").click(function () {
+  $(".js-overlay-campaign-2").fadeOut();
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) {
+  var popup = $(".js-popup-campaign-2");
+  if (e.target != popup[0] && popup.has(e.target).length === 0) {
+    $(".js-overlay-campaign-2").fadeOut();
+  }
+});
+
+$(".js-button-campaign-3").click(function () {
+  $(".js-overlay-campaign-3").fadeIn();
+  $(".js-overlay-campaign-3").addClass("disabled");
+});
+
+$(".js-close-campaign-3").click(function () {
+  $(".js-overlay-campaign-3").fadeOut();
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) {
+  var popup = $(".js-popup-campaign-3");
+  if (e.target != popup[0] && popup.has(e.target).length === 0) {
+    $(".js-overlay-campaign-3").fadeOut();
+  }
+});
+
+$(".js-button-campaign-4").click(function () {
+  $(".js-overlay-campaign-4").fadeIn();
+  $(".js-overlay-campaign-4").addClass("disabled");
+});
+$(".js-close-campaign-4").click(function () {
+  $(".js-overlay-campaign-4").fadeOut();
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) {
+  var popup = $(".js-popup-campaign-4");
+  if (e.target != popup[0] && popup.has(e.target).length === 0) {
+    $(".js-overlay-campaign-4").fadeOut();
+  }
+});
+
+$(".js-button-campaign-5").click(function () {
+  $(".js-overlay-campaign-5").fadeIn();
+  $(".js-overlay-campaign-5").addClass("disabled");
+});
+$(".js-close-campaign-5").click(function () {
+  $(".js-overlay-campaign-5").fadeOut();
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) {
+  var popup = $(".js-popup-campaign-5");
+  if (e.target != popup[0] && popup.has(e.target).length === 0) {
+    $(".js-overlay-campaign-5").fadeOut();
+  }
+});
+$(".js-button-campaign-6").click(function () {
+  $(".js-overlay-campaign-6").fadeIn();
+  $(".js-overlay-campaign-6").addClass("disabled");
+});
+
+$(".js-close-campaign-6").click(function () {
+  $(".js-overlay-campaign-6").fadeOut();
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) {
+  var popup = $(".js-popup-campaign-6");
+  if (e.target != popup[0] && popup.has(e.target).length === 0) {
+    $(".js-overlay-campaign-6").fadeOut();
+  }
+});
+
+//block 7
+
+$(".js-button-campaign-7").click(function () {
+  $(".js-overlay-campaign-7").fadeIn();
+  $(".js-overlay-campaign-7").addClass("disabled");
+});
+
+$(".js-close-campaign-7").click(function () {
+  $(".js-overlay-campaign-7").fadeOut();
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) {
+  var popup = $(".js-popup-campaign-7");
+  if (e.target != popup[0] && popup.has(e.target).length === 0) {
+    $(".js-overlay-campaign-7").fadeOut();
+  }
+});
+//bl8
+
+$(".js-button-campaign-8").click(function () {
+  $(".js-overlay-campaign-8").fadeIn();
+  $(".js-overlay-campaign-8").addClass("disabled");
+});
+
+$(".js-close-campaign-8").click(function () {
+  $(".js-overlay-campaign-8").fadeOut();
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) {
+  var popup = $(".js-popup-campaign-8");
+  if (e.target != popup[0] && popup.has(e.target).length === 0) {
+    $(".js-overlay-campaign-8").fadeOut();
+  }
+});
+
+//9
+
+$(".js-button-campaign-9").click(function () {
+  $(".js-overlay-campaign-9").fadeIn();
+  $(".js-overlay-campaign-9").addClass("disabled");
+});
+
+$(".js-close-campaign-9").click(function () {
+  $(".js-overlay-campaign-9").fadeOut();
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) {
+  var popup = $(".js-popup-campaign-9");
+  if (e.target != popup[0] && popup.has(e.target).length === 0) {
+    $(".js-overlay-campaign-9").fadeOut();
+  }
+});
+
+//10
+
+$(".js-button-campaign-10").click(function () {
+  $(".js-overlay-campaign-10").fadeIn();
+  $(".js-overlay-campaign-10").addClass("disabled");
+});
+
+$(".js-close-campaign-10").click(function () {
+  $(".js-overlay-campaign-10").fadeOut();
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) {
+  var popup = $(".js-popup-campaign-10");
+  if (e.target != popup[0] && popup.has(e.target).length === 0) {
+    $(".js-overlay-campaign-10").fadeOut();
+  }
+});
+
+//11
+
+$(".js-button-campaign-11").click(function () {
+  $(".js-overlay-campaign-11").fadeIn();
+  $(".js-overlay-campaign-11").addClass("disabled");
+});
+
+$(".js-close-campaign-11").click(function () {
+  $(".js-overlay-campaign-11").fadeOut();
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) {
+  var popup = $(".js-popup-campaign-11");
+  if (e.target != popup[0] && popup.has(e.target).length === 0) {
+    $(".js-overlay-campaign-11").fadeOut();
+  }
+});
+
+//12
+
+$(".js-button-campaign-12").click(function () {
+  $(".js-overlay-campaign-12").fadeIn();
+  $(".js-overlay-campaign-12").addClass("disabled");
+});
+
+$(".js-close-campaign-12").click(function () {
+  $(".js-overlay-campaign-12").fadeOut();
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) {
+  var popup = $(".js-popup-campaign-12");
+  if (e.target != popup[0] && popup.has(e.target).length === 0) {
+    $(".js-overlay-campaign-12").fadeOut();
+  }
+});
+
+//13
+
+$(".js-button-campaign-13").click(function () {
+  $(".js-overlay-campaign-13").fadeIn();
+  $(".js-overlay-campaign-13").addClass("disabled");
+});
+
+$(".js-close-campaign-13").click(function () {
+  $(".js-overlay-campaign-13").fadeOut();
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) {
+  var popup = $(".js-popup-campaign-13");
+  if (e.target != popup[0] && popup.has(e.target).length === 0) {
+    $(".js-overlay-campaign-13").fadeOut();
+  }
+});
+//14
+
+$(".js-button-campaign-14").click(function () {
+  $(".js-overlay-campaign-14").fadeIn();
+  $(".js-overlay-campaign-14").addClass("disabled");
+});
+
+$(".js-close-campaign-14").click(function () {
+  $(".js-overlay-campaign-14").fadeOut();
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) {
+  var popup = $(".js-popup-campaign-14");
+  if (e.target != popup[0] && popup.has(e.target).length === 0) {
+    $(".js-overlay-campaign-14").fadeOut();
+  }
+});
+
+//15
+
+$(".js-button-campaign-15").click(function () {
+  $(".js-overlay-campaign-15").fadeIn();
+  $(".js-overlay-campaign-15").addClass("disabled");
+});
+
+$(".js-close-campaign-15").click(function () {
+  $(".js-overlay-campaign-15").fadeOut();
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) {
+  var popup = $(".js-popup-campaign-15");
+  if (e.target != popup[0] && popup.has(e.target).length === 0) {
+    $(".js-overlay-campaign-15").fadeOut();
+  }
+});
+
+//16
+
+$(".js-button-campaign-16").click(function () {
+  $(".js-overlay-campaign-16").fadeIn();
+  $(".js-overlay-campaign-16").addClass("disabled");
+});
+
+$(".js-close-campaign-16").click(function () {
+  $(".js-overlay-campaign-16").fadeOut();
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) {
+  var popup = $(".js-popup-campaign-16");
+  if (e.target != popup[0] && popup.has(e.target).length === 0) {
+    $(".js-overlay-campaign-16").fadeOut();
+  }
+});
+//17
+
+$(".js-button-campaign-17").click(function () {
+  $(".js-overlay-campaign-17").fadeIn();
+  $(".js-overlay-campaign-17").addClass("disabled");
+});
+
+$(".js-close-campaign-17").click(function () {
+  $(".js-overlay-campaign-17").fadeOut();
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) {
+  var popup = $(".js-popup-campaign-17");
+  if (e.target != popup[0] && popup.has(e.target).length === 0) {
+    $(".js-overlay-campaign-17").fadeOut();
+  }
+});
+
+//18
+
+$(".js-button-campaign-18").click(function () {
+  $(".js-overlay-campaign-18").fadeIn();
+  $(".js-overlay-campaign-18").addClass("disabled");
+});
+
+$(".js-close-campaign-18").click(function () {
+  $(".js-overlay-campaign-18").fadeOut();
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) {
+  var popup = $(".js-popup-campaign-18");
+  if (e.target != popup[0] && popup.has(e.target).length === 0) {
+    $(".js-overlay-campaign-18").fadeOut();
+  }
+});
